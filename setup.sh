5c34013498aabc417ae2a64691d2028915f56921
loloolllool <loloolllool@lolcathost.se>
2022-12-18 19:35:37 +0000

    inprogress

--- setup.sh
+++ setup.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
-virsh list --all | grep -v "^ Id" | awk '{print $2}' | xargs -n 1 virsh destroy
+#virsh list --all | grep -v "^ Id" | awk '{print $2}' | xargs -n 1 virsh destroy
 
-seedinitpath="/500gb/iso/seed-init.iso"
-rm seedinitpath -f
+#seedinitpath="/500gb/iso/seed-init.iso"
+#rm seedinitpath -f
 cloud-localds $seedinitpath cloudinit/user-data cloudinit/meta-data
+create_seedimage() {
+    machinename="$1"
+    mkdir -p "cloudinit/tmp"
+    rm "cloudinit/tmp/$machinename" -rf
+    mkdir "cloudinit/tmp/$machinename"
+    cp cloudinit/user-data "cloudinit/tmp/$machinename/user-data"
+    cp cloudinit/meta-data "cloudinit/tmp/$machinename/meta-data"
+    # create user-data and meta-data files
+    sed -i "s/etthostname/$machinename/g" "cloudinit/tmp/$machinename/meta-data"
+    #cloud-localds them both
 
+    #return path to seedinitpath
+}
 create_harddrive() {
-    i=$1
-    machinename="machine$i"
+    echo "creating harddrive2"
+    machinename="$1"
     outputfile=$machinename-disk.qcow2
     outputpath=/2000gb/$outputfile
-    qemu-img convert -f "qcow2" -O qcow2 /500gb/iso/lunar-server-cloudimg-amd64.img $outputpath
-    qemu-img resize --preallocation=metadata $outputpath 25G
+    qemu-img convert -f "qcow2" -O qcow2 /500gb/iso/lunar-server-cloudimg-amd64.img "$outputpath"
+    qemu-img resize --preallocation=metadata "$outputpath" 25G
     echo "Done with: $machinename"
 }
-
-
 create_machine() {
-    i=$1
-    machinename="machine$i"
+    echo "creating vm"
+    machinename="$1"
     outputfile=$machinename-disk.qcow2
     outputpath=/2000gb/$outputfile
 
-    virt-install --name $machinename --disk "$outputpath",device=disk,bus=virtio --disk "$seedinitpath",device=cdrom \
+    virt-install --name "$machinename" --disk "$outputpath",device=disk,bus=virtio --disk "$seedinitpath",device=cdrom \
         --os-variant="ubuntu20.04" \
         --transient \
         --virt-type kvm \
@@ -32,17 +42,27 @@ create_machine() {
         --console pty,target_type=serial \
         --video virtio \
         --import \
-        --noautoconsole 
+        --noautoconsole
 }
 
-echo "Creating harddrives"
-for i in {0..1}; do
-    create_harddrive $i &
+create_vm() {
+    machinename=$1
+    echo "Creating harddrive for $machinename"
+    echo "Creating vm for $machinename"
+    create_seedimage "$machinename"
+    #create_harddrive "$machinename"
+    #create_machine "$machinename"
+}
+for i in {0..4}; do
+    create_vm "vmtest-$i" &
 done
-wait
-for i in {0..1}; do
-    create_machine $i &
-done
+errorCode=$?
+if [ $errorCode -ne 0 ]; then
+    echo "We have an error"
+    # We exit the all script with the same error, if you don't want to
+    # exit it and continue, just delete this line.
+    exit $errorCode
+fi
 # sleep 3
 # clear
 # watch sh listips.sh
